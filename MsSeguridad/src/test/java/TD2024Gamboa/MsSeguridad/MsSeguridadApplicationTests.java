4451ab17eb84a966448910b054327679db5dff08
BrandonGT13 <frank.10.tulumba@gmail.com>
2024-11-05 14:49:00 -0500

    Initial commit

---
+++ MsSeguridad/src/test/java/TD2024Gamboa/MsSeguridad/MsSeguridadApplicationTests.java
@@ -0,0 +1,13 @@
+package TD2024Gamboa.MsSeguridad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsSeguridadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
